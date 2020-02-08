a0122c19ce26330a85022ea024a8849bee2cc288
Victor Maia <srvictormaia@gmail.com>
2020-02-08 18:33:16 -0300

    Split server.js in smaller files
    Former-commit-id: 10855e38dd5046312521c88608d70cf893251228

---
+++ src/server/game.js
@@ -0,0 +1,156 @@
+const db = require("./db.js");
+const TA = require("./../TaelinArena.js");
+
+async function game_server() {
+  // Loads existing games from disk to memory
+  var games = await db.get("games") || [];
+  var turns = [];
+  for (var gid = 0; gid < games.length; ++gid) {
+    var turn_file = (await db.get_str("games.turns."+gid)) || "";
+    turns[gid] = turn_file.split("\n");
+    turns[gid].pop();
+  }
+
+  // Main loop: passes time on all active games
+  setInterval(() => {
+    for (var gid = 0; gid < games.length; ++gid) {
+      add_new_turn(gid);
+    }
+  }, 1000 / TA.GAME_FPS);
+
+  // Creates a new game
+  function create_new_game(game) {
+    var id = games.length;
+    games[id] = {...game, id};
+    turns[id] = [];
+
+    // TODO: persist efficiently
+    db.set("games", games);
+    db.set_str("games.turns."+id, "");
+  };
+
+  // Completes a turn on given game_id
+  function add_new_turn(gid) {
+    var turn_count = turns[gid].length;
+    var game_time = (Date.now() - games[gid].init) / 1000;
+    if ( turn_count <= TA.GAME_DURATION
+      && Date.now() >= games[gid].init) {
+      // If there are no turns, add the first turn
+      if (turn_count === 0) {
+        turns[gid].push("");
+      // Else, end the last turn and add a new one
+      } else {
+        turns[gid][turn_count-1] += "0"; // 'nil'
+        turns[gid].push(""); // new one
+        db.push_str("games.turns."+gid,turns[gid][turn_count-1]+"\n");
+      }
+    }
+  };
+
+  // Adds a input to given game_id
+  function perform_input(gid, player_name, input) {
+    console.log(player_name+" acting on "+gid);
+    var player_id = get_player_id(player_name, games[gid]);
+    var turn_count = turns[gid].length;
+    if ( turn_count > 0
+      && turn_count <= TA.GAME_DURATION
+      && player_id !== null
+      && is_valid_input(input)) {
+      console.log(
+        ("- game="+gid
+        +", turn="+turn_count
+        +", acti="+input));
+      // Creates the turn constructor: (PLAYER_ID+1)|INPUT
+      var turn_ctor = (player_id+1).toString(16);
+      var turn_ctor = turn_ctor + input;
+      turns[gid][turn_count-1] += turn_ctor;
+    }
+  };
+
+  // Gets the player id on given game
+  function get_player_id(name, game) {
+    var players = game.players.split(",");
+    for (var i = 0; i < players.length; ++i) {
+      if (TA.parse_player(players[i]).name === name) {
+        console.log("(" + name + " is " + i + ")");
+        return i;
+      }
+    }
+    return null;
+  };
+
+  // Validates a players object
+  function is_valid_players(players) {
+    // TODO
+    return true;
+  }
+
+  // Validates an input code string
+  function is_valid_input(code) {
+    var ctr = Number(code[0]);
+    if (ctr === 0 && code.length === 3) {
+      return true;
+    }
+    if (ctr >= 1 && ctr <= 3 && code.length === 7) {
+      return true;
+    }
+    if (ctr >= 4 && ctr <= 7) {
+      return true;
+    }
+    return false;
+  };
+
+  // Validates a name string
+  var is_valid_name = (name) => {
+    return typeof name === "string"
+      && /^[a-zA-Z0-9_]{1,32}$/.test(name);
+  };
+
+  // Validates an Ethereum address string
+  var is_valid_address = (addr) => {
+    try {
+      return ethers.utils.getAddress(addr) === addr;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  function make_turn_feed_message(game, turn) {
+    if (game !== TA.NIL_GAME) {
+      var count = turns[game].length;
+
+      // Gather 5 turns around the one demanded by peer
+      var from = Math.min(Math.max((turn||0)-5, 0),count-1); 
+      var to = Math.min((turn||0)+5, count-1);
+
+      // Creates message with game_id, from_turn and inputs
+      var msg = "";
+      msg += ("00000000"+game.toString(16)).slice(-8); 
+      msg += ("00000000"+from.toString(16)).slice(-8);
+      for (var i = from; i < to; ++i) {
+        msg += "1"+turns[game][i];
+      }
+      msg += "0";
+
+      return msg;
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    games,
+    turns,
+    create_new_game,
+    add_new_turn,
+    perform_input,
+    get_player_id,
+    is_valid_players,
+    is_valid_input,
+    is_valid_name,
+    is_valid_address,
+    make_turn_feed_message,
+  }
+};
+
+module.exports = game_server;
--- src/server/server.js
+++ src/server/server.js
@@ -6,138 +6,10 @@ var wrtc = require("wrtc");
 var cors = require("cors");
 var fs = require("fs").promises;
 var path = require("path");
+var db = require("./db.js");
 var ethers = require("ethers");
 var TA = require("./../TaelinArena.js");
-
-// Simple fs database
-var get_str = async (key) => {
-  try {
-    var file_path = path.join("db", key).toLowerCase();
-    return await fs.readFile(file_path, "utf8");
-  } catch (e) {
-    return null;
-  }
-};
-var set_str = async (key, str) => {
-  try {
-    var file_path = path.join("db",key).toLowerCase();
-    await fs.writeFile(file_path, str);
-    return true;
-  } catch (e) {
-    return null;
-  }
-};
-var push_str = async (key, str) => {
-  try {
-    var file_path = path.join("db",key).toLowerCase();
-    await fs.appendFile(file_path, str);
-  } catch (e) {
-    return null;
-  }
-};
-var get = async (key) => {
-  return JSON.parse(await get_str(key));
-};
-var set = async (key, val) => {
-  return set_str(key, JSON.stringify(val));
-};
-
-// Creates a new game
-function create_new_game(game) {
-  var id = games.length;
-  games[id] = {...game, id};
-  turns[id] = [];
-
-  // TODO: persist efficiently
-  set("games", games);
-  set_str("games.turns."+id, "");
-};
-
-// Completes a turn on given game_id
-function add_new_turn(gid) {
-  var turn_count = turns[gid].length;
-  var game_time = (Date.now() - games[gid].init) / 1000;
-  if ( turn_count <= TA.GAME_DURATION
-    && Date.now() >= games[gid].init) {
-    // If there are no turns, add the first turn
-    if (turn_count === 0) {
-      turns[gid].push("");
-    // Else, end the last turn and add a new one
-    } else {
-      turns[gid][turn_count-1] += "0"; // 'nil'
-      turns[gid].push(""); // new one
-      push_str("games.turns."+gid,turns[gid][turn_count-1]+"\n");
-    }
-  }
-};
-
-// Adds a input to given game_id
-function perform_input(gid, player_name, input) {
-  console.log(player_name+" acting on "+gid);
-  var player_id = get_player_id(player_name, games[gid]);
-  var turn_count = turns[gid].length;
-  if ( turn_count > 0
-    && turn_count <= TA.GAME_DURATION
-    && player_id !== null
-    && is_valid_input(input)) {
-    console.log(
-      ("- game="+gid
-      +", turn="+turn_count
-      +", acti="+input));
-    // Creates the turn constructor: (PLAYER_ID+1)|INPUT
-    var turn_ctor = (player_id+1).toString(16);
-    var turn_ctor = turn_ctor + input;
-    turns[gid][turn_count-1] += turn_ctor;
-  }
-};
-
-// Gets the player id on given game
-function get_player_id(name, game) {
-  var players = game.players.split(",");
-  for (var i = 0; i < players.length; ++i) {
-    if (TA.parse_player(players[i]).name === name) {
-      console.log("(" + name + " is " + i + ")");
-      return i;
-    }
-  }
-  return null;
-};
-
-// Validates a players object
-function is_valid_players(players) {
-  // TODO
-  return true;
-}
-
-// Validates an input code string
-function is_valid_input(code) {
-  var ctr = Number(code[0]);
-  if (ctr === 0 && code.length === 3) {
-    return true;
-  }
-  if (ctr >= 1 && ctr <= 3 && code.length === 7) {
-    return true;
-  }
-  if (ctr >= 4 && ctr <= 7) {
-    return true;
-  }
-  return false;
-};
-
-// Validates a name string
-var is_valid_name = (name) => {
-  return typeof name === "string"
-    && /^[a-zA-Z0-9_]{1,32}$/.test(name);
-};
-
-// Validates an Ethereum address string
-var is_valid_address = (addr) => {
-  try {
-    return ethers.utils.getAddress(addr) === addr;
-  } catch (e) {
-    return false;
-  }
-};
+var GS = require("./game.js");
 
 // API
 app.use(cors());
@@ -149,21 +21,21 @@ app.post("/register", async (req, res) => {
   var name = req.body.name;
   var addr = req.body.addr;
 
-  if (!is_valid_name(name)) {
+  if (!gs.is_valid_name(name)) {
     return res.send(JSON.stringify({
       ctr: "err",
       err: "Invalid name.",
     }));
   }
   
-  if (!is_valid_address(addr)) {
+  if (!gs.is_valid_address(addr)) {
     return res.send(JSON.stringify({
       ctr: "err",
       err: "Invalid address.",
     }));
   }
 
-  if ((await get("addr_of."+name)) !== null) {
+  if ((await db.get("addr_of."+name)) !== null) {
     return res.send(JSON.stringify({
       ctr: "err",
       err: "Name taken.",
@@ -172,15 +44,15 @@ app.post("/register", async (req, res) => {
 
   console.log("Registered '"+name+"' as '"+addr+"'.");
 
-  await set("name_of." + addr, name);
-  await set("addr_of." + name, addr);
+  await db.set("name_of." + addr, name);
+  await db.set("addr_of." + name, addr);
   
   res.send(JSON.stringify({ctr: "ok"}));
 });
 
 // Gets the name of an address
 app.post("/name_of", async (req, res) => {
-  var name = await get("name_of."+req.body.addr);
+  var name = await db.get("name_of."+req.body.addr);
   if (!name) {
     res.send(JSON.stringify({
       ctr: "err",
@@ -198,7 +70,7 @@ app.post("/name_of", async (req, res) => {
 app.post("/new_game", async (req, res) => {
   // Validates game name
   var name = req.body.name;
-  if (!is_valid_name(name)) {
+  if (!gs.is_valid_name(name)) {
     return res.send(JSON.parse({
       ctr: "err",
       err: "Invalid game name.",
@@ -207,7 +79,7 @@ app.post("/new_game", async (req, res) => {
 
   // Validates team format
   var players = req.body.players; 
-  if (!is_valid_players(players)) {
+  if (!gs.is_valid_players(players)) {
     return res.send(JSON.stringify({
       ctr: "err",
       err: "Invalid players.",
@@ -216,7 +88,7 @@ app.post("/new_game", async (req, res) => {
 
   // Creates new game and sends success answer
   var init = Date.now() + 2000;
-  create_new_game({name,players,init});
+  gs.create_new_game({name,players,init});
   res.send(JSON.stringify({ctr:"ok"}));
 });
 
@@ -224,13 +96,13 @@ app.post("/new_game", async (req, res) => {
 app.post("/get_game_count", async (req, res) => {
   res.send(JSON.stringify({
     ctr: "ok",
-    count: games.length,
+    count: gs.games.length,
   }));
 });
 
 // Gets a game
 app.post("/get_game", async (req, res) => {
-  var game = games[req.body.id];
+  var game = gs.games[req.body.id];
   if (game) {
     return res.send(JSON.stringify({
       ctr: "ok",
@@ -263,24 +135,10 @@ app.post("/offer", (req, res) => {
 
   // Continuously sends `game_id,from_turn,[input]` to peer
   var turn_feed = setInterval(() => {
-    if (game !== TA.NIL_GAME) {
-      var count = turns[game].length;
-
-      // Gather 5 turns around the one demanded by peer
-      var from = Math.min(Math.max((turn||0)-5, 0),count-1); 
-      var to = Math.min((turn||0)+5, count-1);
-
-      // Creates message with game_id, from_turn and inputs
-      var msg = "";
-      msg += ("00000000"+game.toString(16)).slice(-8); 
-      msg += ("00000000"+from.toString(16)).slice(-8);
-      for (var i = from; i < to; ++i) {
-        msg += "1"+turns[game][i];
-      }
-      msg += "0";
-
+    var msg = gs.make_turn_feed_message(game, turn);
+    if (msg !== null) {
       // Updates the turn demanded by peer
-      turn = Math.min(turn + 3, turns[game].length);
+      turn = Math.min(turn + 3, gs.turns[game].length);
 
       // Sends the turn to peer
       try {
@@ -288,7 +146,6 @@ app.post("/offer", (req, res) => {
       } catch (e) {
         console.log(e);
       };
-
     }
   }, 1000 / TA.GAME_FPS);
 
@@ -377,7 +234,7 @@ app.post("/offer", (req, res) => {
       // Player wants to perform an in-game input.
       case "$":
         if (game !== TA.NIL_GAME) {
-          perform_input(game, name, str.slice(1));
+          gs.perform_input(game, name, str.slice(1));
         }
         break;
 
@@ -396,7 +253,6 @@ app.post("/offer", (req, res) => {
   peer.on("error", (err) => {});
 
   peer.on("close", () => {
-    //console.log(">> "+name+" close <<");
     if (peers[name] === peer) {
       delete peers[name];
     }
@@ -414,23 +270,13 @@ app.post("/answer", (req, res) => {
 // Starts serving files and API on port 80
 app.listen(80);
 
+// Initializes game service
+var gs = await GS();
+
 // Initializes the global state
 var peers = {};
-var games = await get("games") || [];
-var turns = [];
-for (var gid = 0; gid < games.length; ++gid) {
-  var turn_file = (await get_str("games.turns."+gid)) || "";
-  turns[gid] = turn_file.split("\n");
-  turns[gid].pop();
-}
 
 console.log("Server started.");
 
-// Main loop: passes time on all active games
-setInterval(() => {
-  for (var gid = 0; gid < games.length; ++gid) {
-    add_new_turn(gid);
-  }
-}, 1000 / TA.GAME_FPS);
 
 })();
