76bcbb6746fc77816ca8425253401c8f53322372
Maisa <maisa.milena@gmail.com>
2020-05-14 11:48:17 -0300

    Update files that uses Moonad content

--- src/moonad/Moonad-export-helper.js
+++ src/moonad/Moonad-export-helper.js
@@ -81,40 +81,41 @@
   glob(files, {}, async (er, file_names) => {
     var info = files_info();
     
-    var exports_TA_path = "Exports.TaelinArena.fm";
+    var exports_TA_path = "Exports.TA.fm";
     var exports_TA_path = path.join(__dirname, exports_TA_path);
     if (info) {
       const characters = info.filter(entry => {
-        if (entry.name.startsWith("TaelinArena.Thing") && entry.name.endsWith("_fun")){
+        if (entry.name.startsWith("TA.Thing") && entry.name.endsWith("_fun")){
           return entry;
         }
       });
       // 22 Expors.add entries
       var exports_TA_text = 
-      `Exports.TaelinArena: Exports
+      `Exports.TA: Exports
   ( Exports.add<<A: Type> -> List(A)>(List.nil)
   | Exports.add<<A: Type> -> (head: A) -> (tail: List(A)) -> List(A)>(List.cons)
   | Exports.add<(F64.V3 -> F64.V3 -> List(F64.V3) -> List(F64.Segment))>(F64.V3.polygon_to_segments)
   | Exports.add<F64.V3 -> F64.V3 -> F64.V3 -> F64.V3>(F64.V3.look_at)
-  | Exports.add<TaelinArena.Game.PlayerId -> TaelinArena.Game.Game -> F64.V3 >(TaelinArena.Game.get_position_by_pid)
-  | Exports.add<(TaelinArena.Game.Thing -> TaelinArena.Game.Thing) -> TaelinArena.Game.Game -> TaelinArena.Game.Game>(TaelinArena.Game.map_stage)
-  | Exports.add<TaelinArena.Game.PlayerId -> TaelinArena.Game.Input -> TaelinArena.Game.Command>(TaelinArena.Game.Command.new)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.sdir)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key0)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key1)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key2)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key3)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key4)
-  | Exports.add<F64.V3 -> TaelinArena.Game.Input>(TaelinArena.Game.Input.key5)
-  | Exports.add<TaelinArena.Game.Command -> TaelinArena.Game.Game -> TaelinArena.Game.Game>(TaelinArena.exec_command)
-  | Exports.add<TaelinArena.Game.TxFunction -> TaelinArena.Game.PlayerId -> TaelinArena.Game.ModelId -> TaelinArena.Game.ActionId -> TaelinArena.Game.SideId -> Map(F64) -> String -> List(TaelinArena.Game.Light) -> F64 -> F64.V3 -> F64 -> F64 -> F64.V3 -> F64.V3 -> F64.V3 -> F64.V3 -> TaelinArena.Game.Hitbox -> F64 -> F64 -> F64 -> F64.V3 -> List(TaelinArena.Game.Buff) -> List(TaelinArena.Game.Thing) -> List(TaelinArena.Game.Hit) -> Bool -> Bool -> TaelinArena.Game.Thing>(TaelinArena.Game.Thing.new)
-  | Exports.add<(thi: TaelinArena.Game.Thing) -> (new_fun: TaelinArena.Game.TxFunction) -> TaelinArena.Game.Thing>(TaelinArena.Game.set_thing_fun)
-  | Exports.add<(thi: TaelinArena.Game.Thing) -> (new_sid: TaelinArena.Game.SideId) -> TaelinArena.Game.Thing>(TaelinArena.Game.set_thing_sid )
-  | Exports.add<(thi: TaelinArena.Game.Thing) -> (new_pid: TaelinArena.Game.PlayerId) -> TaelinArena.Game.Thing>(TaelinArena.Game.set_thing_pid)
-  | Exports.add<(thi: TaelinArena.Game.Thing) -> (new_pos: F64.V3) -> TaelinArena.Game.Thing>(TaelinArena.Game.set_thing_pos)
-  | Exports.add<(thi: TaelinArena.Game.Thing) -> (new_nam: String) -> TaelinArena.Game.Thing>(TaelinArena.Game.set_thing_nam)
-  | Exports.add<List(TaelinArena.Game.Thing) -> TaelinArena.Game.Game>(TaelinArena.Game.Game.new)
-  | Exports.add<TaelinArena.Game.Game -> TaelinArena.Game.Game>(TaelinArena.exec_turn)
+  | Exports.add<TA.Game.PlayerId -> TA.Game.Game -> F64.V3 >(TA.Game.get_position_by_pid)
+  | Exports.add<(TA.Thing -> TA.Thing) -> TA.Game.Game -> TA.Game.Game>(TA.Game.map_stage)
+  | Exports.add<TA.Game.PlayerId -> TA.Game.Input -> TA.Game.Command>(TA.Game.Command.new)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.sdir)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key0)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key1)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key2)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key3)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key4)
+  | Exports.add<F64.V3 -> TA.Game.Input>(TA.Game.Input.key5)
+  | Exports.add<TA.Game.Command -> TA.Game.Game -> TA.Game.Game>(TA.exec_command)
+  | Exports.add<TA.Game.TxFunction -> TA.Game.PlayerId -> TA.Game.ModelId -> TA.Game.ActionId -> TA.Game.SideId -> Map(F64) -> String -> List(TA.Game.Light) -> F64 -> F64.V3 -> F64 -> F64 -> F64.V3 -> F64.V3 -> F64.V3 -> F64.V3 -> TA.Game.Hitbox -> F64 -> F64 -> F64 -> F64.V3 -> List(TA.Game.Buff) -> List(TA.Thing) -> List(TA.Game.Hit) -> Bool -> Bool -> TA.Thing>(TA.Thing.new)
+  | Exports.add<(thi: TA.Thing) -> (new_fun: TA.Game.TxFunction) -> TA.Thing>(TA.Thing.set_fun)
+  | Exports.add<(thi: TA.Thing) -> (new_sid: TA.Game.SideId) -> TA.Thing>(TA.Thing.set_sid )
+  | Exports.add<(thi: TA.Thing) -> (new_pid: TA.Game.PlayerId) -> TA.Thing>(TA.Thing.set_pid)
+  | Exports.add<(thi: TA.Thing) -> (new_pos: F64.V3) -> TA.Thing>(TA.Thing.set_pos)
+  | Exports.add<(thi: TA.Thing) -> (new_nam: String) -> TA.Thing>(TA.Thing.set_nam)
+  | Exports.add<List(TA.Thing) -> TA.Game.Game>(TA.Game.Game.new)
+  | Exports.add<TA.Game.Game -> TA.Game.Game>(TA.exec_turn)
+  | Exports.add<TA.Game.PlayerId -> TA.Game.Game -> F64.V3>(TA.Game.get_position_by_pid)
       `
       for(var i = 0; i < characters.length; ++i) {
         const exports_code = (i === 0) ? "\n  | Exports.add" : "  | Exports.add";
@@ -127,8 +128,8 @@
         exports_TA_text += i%80 === 0 ? "\n  ;" : ";";
       }
       exports_TA_text += ")";
-      console.log("Exports: ");
-      console.log(exports_TA_text);
+      // console.log("Exports: ");
+      // console.log(exports_TA_text);
       fs.writeFileSync(exports_TA_path, exports_TA_text);
     }
     
